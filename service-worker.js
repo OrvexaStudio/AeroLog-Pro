351248b29641630a7c0feac3a8a24ba0efe04374
OrvexaStudio <phw966j56w@privaterelay.appleid.com>
2026-08-09 01:24:57 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "aerolog-v1";
+
+
+const files = [
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./logo.png"
+];
+
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
